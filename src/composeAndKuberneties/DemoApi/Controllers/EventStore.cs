b2377aba0a1c991a8fcdaf58f1ab060e0fa6c078
Tom <lokas.software@gmail.com>
2021-03-15 09:53:58 +0000

    event store playground

---
+++ src/composeAndKuberneties/DemoApi/Controllers/EventStore.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DemoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventStore : ControllerBase
+    {
+        // GET: api/<EventStore>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST api/<EventStore>
+        [HttpPost]
+        public void Post()
+        {
+            //TODO: generate random events blah
+        }
+    }
+}
